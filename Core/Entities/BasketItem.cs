31cdcad84ebb3569b6d25c34e080bc9a1131fd40
Shivesh Dindayal <shivy101@live.com>
2021-01-09 17:31:05 +0100

    checkout

--- Core/Entities/BasketItem.cs
+++ Core/Entities/BasketItem.cs
@@ -2,7 +2,7 @@ namespace Core.Entities
 {
     public class BasketItem
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string ProductName { get; set; }
 
         public decimal Price { get; set; }
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,14 +12,33 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+  constructor(private shopService: ShopService,
+              private activateRoute: ActivatedRoute,
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
     this.bcService.set('@productDetails', '');
   }
 
   ngOnInit(): void {
     this.loadProduct();
   }
+
+  additemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuanity() {
+    this.quantity++;
+  }
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+
+  }
+
 
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
